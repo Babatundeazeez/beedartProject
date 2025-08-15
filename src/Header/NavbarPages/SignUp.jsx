365d16ff85a0a3be26d53ad421ed1729bfe7f32c
Babatundeazeez <techdevmind22@gmail.com>
2025-08-15 12:43:29 +0100

    signUp update

--- src/Header/NavbarPages/SignUp.jsx
+++ src/Header/NavbarPages/SignUp.jsx
@@ -56,46 +56,52 @@ const SignUp = () => {
 
   return (
     <div>
-      <div className='container'>
-      <h4 className='mt-5'>Welcome to SignUp page</h4>
-
-      <form action="" style={{marginTop : "100px"}} className='form-control w-50 border p-4' onSubmit={handleSubmit(submitForm)}>
+      <div className='container my-5'>
+      
+      <div className='row justify-content-center' style={{marginTop : "100px"}}>
+        <div className='col-12 col-sm-8 col-md-6 col-lg-4'>
+        <h4 className='text-center mb-4'>Welcome to SignUp page</h4>
+        <form action="" className='form-control bg-light p-4' onSubmit={handleSubmit(submitForm)}>
         <p className='d-flex justify-content-center'>Kindly sign up.......</p>
 
-        <div className='mt-3'>
-          <label htmlFor="name">First Name:</label>
+        <div className='mb-3'>
+          <label htmlFor="name" className='form-label'>First Name:</label>
           <input className='form-control mt-1' type="text" name="" id="name" placeholder='Enter your name' {...register('name')} /> 
         </div>
 
-        <div className='mt-1'>
-          <label htmlFor="name">Last Name:</label>
-          <input className='form-control mt-1' type="text" name="" id="Lname" placeholder='Enter your last name' {...register('Lname')} />
+        <div className='mb-3'>
+          <label htmlFor="name" className='form-label'>Last Name:</label>
+          <input className='form-control' type="text" name="" id="Lname" placeholder='Enter your last name' {...register('Lname')} />
         </div>
 
-        <div className='mt-1'>
-          <label htmlFor="name">Middle Name:</label>
-          <input className='form-control mt-1' type="text" name="" id="Mname" placeholder='Enter your middle name' {...register('Mname')}/>
+        <div className='mb-3'>
+          <label htmlFor="name" className='form-label'>Middle Name:</label>
+          <input className='form-control' type="text" name="" id="Mname" placeholder='Enter your middle name' {...register('Mname')}/>
         </div>
         
-        <div className='mt-1'>
-          <label htmlFor="name">Address:</label>
-          <input className='form-control mt-1' type="name" id="address" placeholder='Enter your email address' {...register('address')} />
+        <div className='mb-3'>
+          <label htmlFor="name" className='form-label'>Address:</label>
+          <input className='form-control' type="name" id="address" placeholder='Enter your email address' {...register('address')} />
         </div>
 
-        <div className='mt-1'>
-          <label htmlFor="name">Email:</label>
-          <input className='form-control mt-1' type="email" name="" id="email" placeholder='Enter your email address' {...register('email')} />
+        <div className='mb-3'>
+          <label htmlFor="name" className='form-label'>Email:</label>
+          <input className='form-control' type="email" name="" id="email" placeholder='Enter your email address' {...register('email')} />
           <p style={{color: "red"}}>{errors.email && errors.email.message}</p>
         </div>
 
-        <div className='mt-1'>
-          <label htmlFor="name">Password:</label>
-          <input className='form-control mt-1' type="password" name="" id="password" placeholder='Enter your email password' {...register('password')} />
+        <div className='mb-3'>
+          <label htmlFor="name" className='form-label'>Password:</label>
+          <input className='form-control' type="password" name="" id="password" placeholder='Enter your email password' {...register('password')} />
           {/* <p style={{color: "red"}}>{errors.password && errors.password.message}</p> */}
         </div>
 
-        <button className='btn btn-primary mt-3'>Sign up</button>
+        <button className='btn btn-primary w-100'>Sign up</button>
       </form>
+        </div>
+      </div>
+
+     
 
       </div>
         
