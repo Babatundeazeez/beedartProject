e24d049b7f6765c2f9a7a3b59242813f00bf1ff9
Babatundeazeez <techdevmind22@gmail.com>
2025-08-15 17:35:27 +0100

    input

--- src/Header/Frames/BlogContet.jsx
+++ src/Header/Frames/BlogContet.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const BlogContet = () => {
+    const {register, handleSubmit} = useForm()
+
+    const submitBlog = (data) =>{
+        console.log(data);
+        
+    }
   return (
     <div className='row'>
         <div className='col-sm-6 col-md-6'>
         <div className='card shadow'>
             <div className='card-body'>
-                <form action="" className='form'>
-                    <div>
+                <form action="" className='form' onSubmit={handleSubmit(submitBlog)}>
+                    <div className='mb-3'>
                         <label htmlFor="" className='form-label'>Title:</label>
-                        <input type="text" className='form-control' placeholder='Enter title' />
+                        <input type="text" id='title' className='form-control' placeholder='Enter title' {...register("title")} />
                     </div>
-                    <div>
+                    <div className='mb-3'>
                         <label htmlFor="" className='form-label'>Header:</label>
-                        <input type="text" className='form-control' placeholder='Enter' />
+                        <input type="text" id='header' className='form-control' placeholder='Enter' {...register("header")} />
                     </div>
-                    <div>
+                    <div className='mb-3'>
                         <label htmlFor="" className='form-label'>Title:</label>
-                        <textarea name="" id="message" className='form-control' >
+                        <textarea name="" id="message" className='form-control' {...register("message")} >
 
                         </textarea>
                     </div>
-                    <div>
+                    <div className='mb-3'>
                         <button className='btn btn-primary'>Creat Blog Post</button>
                     </div>
                 </form>
